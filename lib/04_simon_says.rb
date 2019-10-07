58b5ea5ea3cfa0db3892680fc81557b8640d0ef1
Lucas Grandviergne <lucasgdve@gmail.com>
2019-10-07 22:29:30 +0200

    lucas commit

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -1,3 +1,26 @@
-def echo
-    return "hello"
+def echo(name)
+    return "#{name}"
 end
+
+def shout(name)
+   return name.upcase
+end
+
+def repeat(name,repetitions=2)
+    return ([name] * repetitions).join(" ")
+end
+
+def start_of_word(n, nu)
+   return n[0]
+end
+
+
+def start_of_word(n,nu)
+    return n[0..2]
+end
+
+def first_word(h)
+    h.delete "World"
+end
+    
+
